b26734a1676eb5cd17ec51722487c203a8902beb
George Obeid <54781107+Georgeobeid@users.noreply.github.com>
2021-11-08 20:33:02 -0300

    feat(fix-some-errors): refactor update dependencies and environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBIZ5d4lIx5xlXdifYvcIAKT1PnZ9AhRIA",
+    authDomain: "produtos-8dae6.firebaseapp.com",
+    databaseURL: "https://produtos-8dae6-default-rtdb.firebaseio.com",
+    projectId: "produtos-8dae6",
+    storageBucket: "produtos-8dae6.appspot.com",
+    messagingSenderId: "967698174848",
+    appId: "1:967698174848:web:439f8441d66ed1bd5ea765",
+    measurementId: "G-SCQ1Y5CF9E"
+  }
 };
